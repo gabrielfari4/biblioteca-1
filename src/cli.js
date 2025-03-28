429f9fc62815514fd5f116ca6be657e5ec16459a
gabrielfari4 <gabrielfaria88@gmail.com>
2025-03-28 11:05:17 -0300

    :sparkles: feat: adicionando bibliotecas pra CLI

--- src/cli.js
+++ src/cli.js
@@ -4,20 +4,55 @@ import fs from "fs";
 import trataErros from "./erros/funcoesErro.js";
 import { contaPalavras } from "./index.js";
 import { montaSaidaArquivo } from "./helpers.js";
+import { Command } from 'commander';
+import path from "path";
 
-const caminhoArquivo = process.argv;
+const program = new Command();
+
+program
+    .version('0.0.1')
+    .option('-t, --texto <string>', 'caminho do texto a ser processado')
+    .option('-d, --destino <string>', 'caminho da pasta onde salvar o arquivo de resultados')
+    .action((options) => {
+        const { texto, destino } = options;
+
+        if (!texto || !destino) {
+            console.error('erro: favor inserir caminho de origem e destino')
+            program.help()
+            return
+        }
+
+        const caminhoTexto = path.resolve(texto)
+        const caminhoDestino = path.resolve(destino)
+
+        try {
+            processaArquivo(caminhoTexto, caminhoDestino)
+            console.log('texto processado com sucesso');
+            
+        } catch (error) {
+            console.log('ocorreu um erro no processamento: ', error);
+            
+        }
+    })
+
+program.parse()
+
+/* const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
-const endereco = caminhoArquivo[3];
+const endereco = caminhoArquivo[3]; */
 
-fs.readFile(link, "utf-8", (erro, texto) => {
-    try {
-        if (erro) throw erro;
-        const resultado = contaPalavras(texto);
-        criaESalvaArquivos(resultado, endereco);
-    } catch (erro) {
-        trataErros(erro);
-    }
-});
+function processaArquivo (texto, destino) {
+    fs.readFile(texto, "utf-8", (erro, texto) => {
+        try {
+            if (erro) throw erro;
+            const resultado = contaPalavras(texto);
+            criaESalvaArquivos(resultado, destino);
+        } catch (erro) {
+            trataErros(erro);
+        }
+    })
+}
+
 
 /* const criaESalvaArquivos = async (listaPalavras, endereco) => {
     const arquivoNovo = `${endereco}/resultado.txt`;
